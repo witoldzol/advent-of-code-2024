c0e13336be03d369750d9f5d03cc84ab93ed3534
Witold Zolnowski <blue3656@gmail.com>
2025-06-03 16:33:19 +0100

    day4 part 1

--- d4.py
+++ d4.py
@@ -7,63 +7,15 @@ for l in input:
     matrix.append(x)
 
 xmas_count  = 0
-for x, row in enumerate(matrix):
-    for y, c in enumerate(row):
-        if c == 'X':
-            # left to right
-            if y + 3 < len(row):
-                m = row[y+1]
-                a = row[y+2]
-                s = row[y+3]
-                if m == 'M' and a == 'A' and s == 'S':
-                    xmas_count += 1
-            # right to left
-            if y - 3 >= 0:
-                m = row[y-1]
-                a = row[y-2]
-                s = row[y-3]
-                if m == 'M' and a == 'A' and s == 'S':
-                    xmas_count += 1
-            # up
-            if x - 3 >= 0:
-                m = row[x-1]
-                a = row[x-2]
-                s = row[x-3]
-                if m == 'M' and a == 'A' and s == 'S':
-                    xmas_count += 1
-            # down
-            if x + 3 < len(matrix):
-                m = row[x+1]
-                a = row[x+2]
-                s = row[x+3]
-                if m == 'M' and a == 'A' and s == 'S':
-                    xmas_count += 1
-            # diagonal to up right
-            if x - 3 >= 0 and y + 3 < len(row):
-                m = matrix[x-1][y+1]
-                a = matrix[x-2][y+2]
-                s = matrix[x-3][y+3]
-                if m == 'M' and a == 'A' and s == 'S':
-                    xmas_count += 1
-            # diagonal to up left
-            if x - 3 >= 0 and y - 3 >= 0:
-                m = matrix[x-1][y-1]
-                a = matrix[x-2][y-2]
-                s = matrix[x-3][y-3]
-                if m == 'M' and a == 'A' and s == 'S':
-                    xmas_count += 1
-            # diagonal to down left
-            if x + 3 < len(matrix) and y - 3 >= 0:
-                m = matrix[x+1][y-1]
-                a = matrix[x+2][y-2]
-                s = matrix[x+3][y-3]
-                if m == 'M' and a == 'A' and s == 'S':
-                    xmas_count += 1
-            # diagonal to down right
-            if x + 3 < len(matrix) and y + 3 < len(row):
-                m = matrix[x+1][y+1]
-                a = matrix[x+2][y+2]
-                s = matrix[x+3][y+3]
-                if m == 'M' and a == 'A' and s == 'S':
-                    xmas_count += 1
+for r in range(len(matrix)):
+    for c in range(len(matrix[0])):
+        if matrix[r][c] != "X": continue
+        for rd in (-1,0,1):
+            for cd in (-1,0,1):
+                if rd == 0 and cd == 0: continue
+                if 0 <= r + rd * 3 < len(matrix) and 0 <= c + cd * 3 < len(matrix[0]):
+                    if matrix[r + rd][c + cd] == 'M' and matrix[r + rd * 2][c + cd * 2] == 'A' and matrix[r + rd * 3][c + cd * 3] == 'S':
+                        xmas_count += 1
 print(xmas_count)
+
+
