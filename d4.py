8d7e4c21e730b2b699ac85689723c2247752899e
Witold Zolnowski <blue3656@gmail.com>
2025-06-03 18:07:00 +0100

    day4 part 2

--- d4.py
+++ d4.py
@@ -1,6 +1,8 @@
-"XMAS"
+# find "XMAS"
+# M * S
+# * A *
+# M * S
 input = open('input4')
-input = open('testinput')
 matrix = []
 for l in input:
     x = list(l.rstrip())
@@ -9,13 +11,12 @@ for l in input:
 xmas_count  = 0
 for r in range(len(matrix)):
     for c in range(len(matrix[0])):
-        if matrix[r][c] != "X": continue
-        for rd in (-1,0,1):
-            for cd in (-1,0,1):
-                if rd == 0 and cd == 0: continue
-                if 0 <= r + rd * 3 < len(matrix) and 0 <= c + cd * 3 < len(matrix[0]):
-                    if matrix[r + rd][c + cd] == 'M' and matrix[r + rd * 2][c + cd * 2] == 'A' and matrix[r + rd * 3][c + cd * 3] == 'S':
-                        xmas_count += 1
+        if matrix[r][c] != "A": continue
+            # check boundaries
+        if not(r + 1 < len(matrix) and c + 1 < len(matrix[0]) and r - 1 >= 0 and c - 1 >= 0): continue
+        if (matrix[r-1][c-1] == 'M' and matrix[r-1][c+1] == 'M' and matrix[r+1][c-1] == 'S' and matrix[r+1][c+1] == 'S') or \
+            (matrix[r-1][c-1] == 'S' and matrix[r-1][c+1] == 'S' and matrix[r+1][c-1] == 'M' and matrix[r+1][c+1] == 'M') or \
+            (matrix[r-1][c-1] == 'M' and matrix[r-1][c+1] == 'S' and matrix[r+1][c-1] == 'M' and matrix[r+1][c+1] == 'S') or \
+            (matrix[r-1][c-1] == 'S' and matrix[r-1][c+1] == 'M' and matrix[r+1][c-1] == 'S' and matrix[r+1][c+1] == 'M'):
+            xmas_count += 1
 print(xmas_count)
-
-
