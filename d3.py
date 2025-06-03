2b068c8d14ab721384d3f1d22016a0dfb98c779c
Witold Zolnowski <blue3656@gmail.com>
2025-06-03 14:56:03 +0100

    day3 part 2

--- d3.py
+++ d3.py
@@ -2,11 +2,15 @@ import re
 
 f = open('input3')
 sum = 0
+do = True
 for l in f:
-    m = re.findall(r"mul\((\d{0,3}),(\d{0,3})\)", l)
-    for x,y in m:
-        sum += int(x) * int(y)
-print('sum')
+    matches = re.findall(r"(mul)\((\d{0,3}),(\d{0,3})\)|(do)\(\)|(don't)\(\)", l)
+    for m in matches:
+        if m[0] and m[0] == 'mul':
+            if do:
+                sum += int(m[1]) * int(m[2])
+        elif m[3]:
+            do = True
+        elif m[4]:
+            do = False
 print(sum)
-
-
