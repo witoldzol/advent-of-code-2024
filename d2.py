8e1d9bb7fafc891f7253eb1ba04faecabc107f16
Witold Zolnowski <blue3656@gmail.com>
2025-06-03 13:41:20 +0100

    day 2 part 2

--- d2.py
+++ d2.py
@@ -1,7 +1,9 @@
-def is_safe(inputs: list[str]) -> int:
+def is_safe(inputs: list[str], skip: int = -1) -> int:
     prev = None
     is_increasing = None
-    for x in inputs:
+    for idx, x in enumerate(inputs):
+        if skip == idx:
+            continue
         if prev is None:
             prev = x
             continue
@@ -9,8 +11,26 @@ def is_safe(inputs: list[str]) -> int:
             is_increasing = int(x) > int(prev)
         diff = abs(int(x) - int(prev))
         if diff < 1 or diff > 3:
+            if skip == -1:
+                first_try =  is_safe(inputs, skip=idx)
+                second_try = 0
+                if idx - 1 >= 0:
+                    second_try =  is_safe(inputs, skip=idx - 1)
+                third_try = 0
+                if idx - 2 >= 0:
+                    third_try =  is_safe(inputs, skip=idx - 2)
+                return first_try or second_try or third_try
             return 0
         if is_increasing != (int(x) > int(prev)):
+            if skip == -1:
+                first_try =  is_safe(inputs, skip=idx)
+                second_try = 0
+                if idx - 1 >= 0:
+                    second_try =  is_safe(inputs, skip=idx - 1)
+                third_try = 0
+                if idx - 2 >= 0:
+                    third_try =  is_safe(inputs, skip=idx - 2)
+                return first_try or second_try or third_try
             return 0
         prev = x
     return 1
