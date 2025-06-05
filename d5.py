bd8020e25de68fbc0c9fda81e1e103973262c264
Witold Zolnowski <blue3656@gmail.com>
2025-06-05 08:26:24 +0100

    day5 part 2

--- d5.py
+++ d5.py
@@ -1,4 +1,20 @@
 f = open('./input5')
+
+def swap(rules: dict[str,set[str]], input: list[str]) -> None:
+    for idx, x in enumerate(input):
+        if x in rules:
+            rule: set[str] = rules[x]
+            # check all elements before the x, if they make run invalid
+            for y in range(idx):
+                #print(f'is {y} in {rule}?')
+                if input[y] in rule:
+                    # swap
+                    input[idx], input[y] = input[y], input[idx]
+
+def fix_order(page_rules: dict[str,set[str]], input: list[str]) -> None:
+    while is_correct_order(page_rules, input) < 0:
+        swap(page_rules, input)
+
 def is_correct_order(rules: dict[str,set[str]], input: list[str]) -> int:
     for idx, x in enumerate(input):
         if x in rules:
@@ -7,7 +23,7 @@ def is_correct_order(rules: dict[str,set[str]], input: list[str]) -> int:
             for y in range(idx):
                 #print(f'is {y} in {rule}?')
                 if input[y] in rule:
-                    return 0
+                    return -1
     #print('in order:\n', input)
     mid = len(input) // 2
     #print('mid', mid)
@@ -16,6 +32,7 @@ def is_correct_order(rules: dict[str,set[str]], input: list[str]) -> int:
 
 page_rules = {}
 sum = 0
+fixed_sum = 0
 is_rules = True
 for l in f:
     if l == '\n':
@@ -30,6 +47,13 @@ for l in f:
             page_rules[before] = {after}
     else:
         input = l.rstrip().split(',')
-        sum += is_correct_order(page_rules, input)
-print(sum)
+        middle_page = is_correct_order(page_rules, input)
+        if middle_page < 0:
+            fix_order(page_rules, input)
+            fixed_middle_page = is_correct_order(page_rules, input)
+            fixed_sum += fixed_middle_page
+        else:
+            sum += middle_page
+# print(sum)
+print(fixed_sum)
 
