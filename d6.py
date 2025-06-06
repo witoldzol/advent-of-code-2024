fc697ab4a000c603e79781a9c9b07cf88267ceda
Witold Zolnowski <blue3656@gmail.com>
2025-06-06 08:17:36 +0100

    day 6 wip2

--- d6.py
+++ d6.py
@@ -1,8 +1,11 @@
 from enum import Enum
 
-class Direction():
-    def __init__(self) -> None:
+class Guard:
+    def __init__(self, x, y) -> None:
         self.current = "up"
+        self.x = x
+        self.y = y
+
     def get_direction(self):
         if self.current == "up":
             return (-1,0)
@@ -14,6 +17,7 @@ class Direction():
             return (0,-1)
         else:
             raise Exception('Wrong direction!')
+
     def change_direction(self):
         if self.current == "up":
             self.current = "right"
@@ -24,8 +28,22 @@ class Direction():
         else:
             self.current = "up"
 
+    def __repr__(self) -> str:
+        return f"Guard({self.x}, {self.y})"
 
 f = open('./testinput')
-
+grid = []
 for l in f:
-    print(list(l.rstrip()))
+    grid.append(list(l.rstrip()))
+
+# init guard with start location
+guard = None
+POSTITION_MARKER = "^"
+for r in range(len(grid)):
+    for c in range(len(grid[0])):
+        if grid[r][c] == POSTITION_MARKER:
+            g = Guard(r,c)
+if g is None:
+    raise Exception("Failed to init guard")
+
+print(g)
