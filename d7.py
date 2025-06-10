2d1b7f5cb387b057a1f8352a3ba6c1714282ac13
Witold Zolnowski <blue3656@gmail.com>
2025-06-10 19:01:20 +0100

    day 7 part 2 done

--- d7.py
+++ d7.py
@@ -1,19 +1,25 @@
 f = open('input7')
 # f = open('testinput')
 
-def calc(numbers, target):
-    if len(numbers) == 0:
-        return target == 0
-    add = target - numbers[-1]
-    multi = target//numbers[-1]
-    return calc(numbers[:-1], add) or (target%numbers[-1]==0 and calc(numbers[:-1], multi))
+def calc(numbers, target, sum):
+    if not numbers:
+        return target == sum 
+    add = sum + numbers[0]
+    if sum == 0:
+        multi = numbers[0]
+        concat = numbers[0]
+    else:
+        multi = sum * numbers[0]
+        concat = int(str(sum) + str(numbers[0]))
+    return calc(numbers[1:], target, add) or calc(numbers[1:], target, multi) or calc(numbers[1:], target, concat)
 
 sum = 0
 for l in f:
     target, input = l.rstrip().split(':')
     inputs = [int(x) for x in input.split(' ') if x]
     target = int(target)
-    if calc(inputs, target):
+    res = calc(inputs, target, 0)
+    if res:
         sum += target
 
 print(sum)
