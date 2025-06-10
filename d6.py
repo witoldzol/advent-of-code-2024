885559e1c78a073bd5776fd46804a65072c0fbf4
Witold Zolnowski <blue3656@gmail.com>
2025-06-10 08:18:04 +0100

    ignore predtictions if obstacle ahead, handle preditions when moving right

--- d6.py
+++ d6.py
@@ -30,6 +30,13 @@ class Guard:
     def __repr__(self) -> str:
         return f"Guard({self.x}, {self.y})"
 
+    def is_obstacle_ahead(self, grid):
+        x, y = self.move()
+        if 0 <= self.x + x < len(grid) and 0 <= self.y + y < len(grid[0]):
+            if grid[self.x + x][self.y + y] == "#":
+                return True
+        return False
+
 f = open('input6')
 f = open('testinput')
 grid = []
@@ -52,7 +59,7 @@ for r in range(len(grid)):
 if g is None:
     raise Exception("Failed to init guard")
 
-def is_on_right(i: int, obstacles:list[int]) -> int:
+def next_obstacle(i: int, obstacles:list[int]) -> int:
     sorted_obstacles = sorted(obstacles)
     for x in sorted_obstacles:
         if x > i:
@@ -66,14 +73,21 @@ def traverse(g: Guard, matrix: list[list[str]], x_axis: dict[int,list[int]], y_a
         # check for alternatives before we move
         # alternatives depend on the direction we are facing
         # up -> right : y_axis
-        # right -> down: x_axis
+        if not g.is_obstacle_ahead(grid):
+            if g.current == "up":
+                if g.x in x_axis:
+                    next_blockade = next_obstacle(g.y, x_axis[g.x])
+                    if next_blockade != -1:
+                        print(f'we could go right from position {g.x} {g.y} to block {g.x}, {next_blockade}')
+            # right -> down: x_axis
+            if g.current == "right":
+                # if we go right, that means we traverse y axis, and potential pivot is down, so we need to check obstacles in x axis
+                if g.y in y_axis:
+                    next_blockade = next_obstacle(g.x, y_axis[g.y])
+                    if next_blockade != -1:
+                        print(f'we could go down from position {g.x} {g.y} to block {next_blockade}, {g.y}')
         # down -> left : y_axis
         # left -> up : x_axis
-        if g.current == "up":
-            if g.x in x_axis:
-                next_blockade_on_right = is_on_right(g.y, x_axis[g.x])
-                if next_blockade_on_right != -1:
-                    print(f'we could go right from position {g.x} {g.y} to block {g.x}, {next_blockade_on_right}')
         x, y = g.move()
         if 0 <= g.x + x < len(grid) and 0 <= g.y + y < len(grid[0]):
             if grid[g.x + x][g.y + y] != '#':
