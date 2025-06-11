e11476a6b303e10085be2f863851c5646ca57436
Witold Zolnowski <blue3656@gmail.com>
2025-06-11 07:13:41 +0100

    day 8 part 1 done

--- d8.py
+++ d8.py
@@ -1,5 +1,6 @@
-# f = open('input8')
-f = open('testinput')
+f = open('input8')
+# f = open('testinput')
+locations = set()
 grid = []
 antenas = {}
 for l in f:
@@ -19,7 +20,16 @@ for a_type,coords in antenas.items():
                 continue
             ix,iy = coords[i]
             jx,jy = coords[j]
-            print(a_type, ix,iy, '->', jx,jy)
+            # print(a_type, ix,iy, '->', jx,jy)
+            dx = ix - jx
+            dy = iy - jy
+            if 0<=ix+dx<len(grid) and 0<=iy+dy<len(grid[0]):
+                # if grid[ix+dx][iy+dy] == '.':
+                grid[ix+dx][iy+dy] = '#'
+                locations.add((ix+dx,iy+dy))
+            # print(a_type, ix-jx,iy-jy)
+for l in grid:
+    print(''.join(l))
 
-
+print('total ', len(locations))
 
