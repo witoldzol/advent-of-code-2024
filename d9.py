248f2e2b2540349ac9484ec581c0ddc9ef4c55e6
Witold Zolnowski <blue3656@gmail.com>
2025-06-12 10:40:21 +0100

    speed up by 3x, remove profile, still wrong answer

--- d9.py
+++ d9.py
@@ -1,6 +1,3 @@
-import cProfile
-import pstats
-
 f = open('testinput')
 f = open('input9')
 input = f.read().strip()
@@ -27,13 +24,17 @@ def generate_blocks(input:str) -> list[str]:
             current_idx += empty_block_size
     return blocks
 
-def move_file(blocks) -> int:
+def compact_entire_files(blocks) -> None:
     for file, file_info in reversed(FILES.items()):
+        # print(f'processing file {file}')
         file_idx, file_size = file_info
         for sector_index, sector_info  in enumerate(EMPTY_SECTORS):
+            if sector_info is None:
+                continue
             empty_idx, empty_size = sector_info
             # if we can fit the file
             if file_size <= empty_size:
+                # print(f'found block to fits {file}, block idx {empty_idx}, size {empty_size}')
                 file = blocks[file_idx]
                 # move file to empty sector
                 for i in range(empty_idx, empty_idx + file_size):
@@ -41,22 +42,17 @@ def move_file(blocks) -> int:
                 # remove file from previous location
                 for i in range(file_idx, file_idx + file_size):
                     blocks[i] = '.'
-                # delete file from dict
-                del FILES[file]
                 # update empty sector
                 if file_size == empty_size:
-                    del EMPTY_SECTORS[sector_index]
+                    EMPTY_SECTORS[sector_index] = None
                 else:
                     new_start_idx = empty_idx + file_size
                     new_size = empty_size - file_size
                     EMPTY_SECTORS[sector_index] = (new_start_idx, new_size)
-                return 1
-    return 0
+                break
+            # else:
+            #     print(f'did not find empty block for file {file}')
 
-def compact_entire_files(blocks: list[str]) -> None:
-    ret = 1
-    while FILES and ret == 1:
-        ret = move_file(blocks)
 
 def gen_checksum(blocks: list[str]) -> int:
     checksum = 0
@@ -66,20 +62,14 @@ def gen_checksum(blocks: list[str]) -> int:
         checksum += i * int(x)
     return checksum
 
-# profile = cProfile.Profile()
-# profile.enable()
-
 blocks = generate_blocks(input)
-# expected = '00...111...2...333.44.5555.6666.777.888899'
+expected = '00...111...2...333.44.5555.6666.777.888899'
 # assert expected == ''.join(blocks)
 
+# print(FILES)
 compact_entire_files(blocks)
 # assert '00992111777.44.333....5555.6666.....8888..' == ''.join(blocks), ''.join(blocks)
 
 print('checksum')
 print(gen_checksum(blocks))
-# profile.disable()
-# profile.dump_stats("stats.prof")
-# stats = pstats.Stats("stats.prof")
-# stats.print_stats()
 # 8444425634594 = too high
