5b6b7eb646bf2c477ede7d76da4be2d5bc52348b
Witold Zolnowski <blue3656@gmail.com>
2025-06-12 17:27:36 +0100

    day 10 setup

---
+++ d10.py
@@ -0,0 +1,8 @@
+f = open('testinput')
+grid = []
+for l in f:
+    grid.append(list(l.rstrip()))
+for l in grid:
+    print(l)
+def score_traihead(start: tuple[int,int], gridlist: list[list[str]])->int:
+    return 1
