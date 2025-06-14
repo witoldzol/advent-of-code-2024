77aa839348e246e6cf4e1a0730fa5a5acc29175b
Witold Zolnowski <blue3656@gmail.com>
2025-06-14 22:35:42 +0100

    witp

--- d11.py
+++ d11.py
@@ -27,6 +27,21 @@ def insert_after(node: Node, to_be_inserted: Node) -> None:
     to_be_inserted.prev = node
     node.next = to_be_inserted
 
+def replace_node(node: Node, new_node: Node) -> None:
+    print('replacing node ', node, ' with node ' , new_node)
+    new_node.prev = node.prev
+    new_node.next = node.next # 99
+    node.prev.next = new_node
+    node.next.prev = new_node
+
+def split_node(old_node: Node, new_left: Node, new_right: Node) -> None:
+    print('replacing node ', old_node, ' with nodes ' , new_left , 'and ', new_right)
+    new_left.prev = old_node.prev
+    old_node.prev.next = new_left
+    new_left.next = new_right
+    new_right.prev = new_left
+    new_right.next = old_node.next
+
 # initialize the list with input
 current = start
 for s in stones:
@@ -38,30 +53,37 @@ for s in stones:
 # for n in start.next:
 n = start.next
 while n:
-    print(f'transforming node {n}')
+    if n is None or n.val is None:
+        break
+    print(f'transforming node{n}')
     if n.val == 0:
         n.val = 1
+        n = n.next
+    # split into two halfs
     elif len(str(n)) % 2 == 0:
+        print('splitting into two halfs num ' , str(n))
         num = str(n.val)
         mid = len(num) // 2
         left = num[:mid]
         if len(left) > 1:
             left = left.rstrip('0')
+        print('left ', left)
         right = num[mid:]
         if len(right) > 1:
             right = right.rstrip('0')
-        # print(f'SPLITTING {num}')
-        # print('left')
-        # print(left)
-        # print('right')
-        # print(right)
+        print('right ', right)
         left_node = Node(int(left))
         right_node = Node(int(right))
-        insert_after(n, left_node)
-        insert_after(left_node, right_node)
+        # if right_node.val == 0:
+        #     import pudb;pu.db
+        split_node(n, left_node, right_node)
         n = right_node.next
     else:
+        print('multiply by 2024 ', n)
         n.val = n.val * 2024
-
+        n = n.next
+sol = []
+for n in start.next:
+    sol.append(n.val)
 for n in start.next:
     print(n)
