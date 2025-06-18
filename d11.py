347b3b1e67733dba245f1a6d674203853bafdcee
Witold Zolnowski <blue3656@gmail.com>
2025-06-18 07:50:24 +0100

    add profiler

--- d11.py
+++ d11.py
@@ -1,14 +1,16 @@
+import cProfile
+import pstats
 from collections import deque
 from functools import lru_cache
 
 
 stones = open('testinput').read().strip().split(' ')
-# stones = open('input11').read().strip().split(' ')
+stones = open('input11').read().strip().split(' ')
 
 @lru_cache(maxsize=None)
-def transform(n: int) -> tuple[int]:
+def transform(n: int) -> list[int]:
     if n == 0:
-        return (1,)
+        return [1]
     # split into two halfs
     elif len(str(n)) % 2 == 0:
         #print('splitting into two halfs num ' , str(n))
@@ -26,13 +28,13 @@ def transform(n: int) -> tuple[int]:
         if right == '':
             right = 0
         #print('right ', right)
-        return (int(left), int(right),)
+        return [int(left), int(right)]
     else:
         # #print('multiply by 2024 ', n)
-        return (n * 2024,)
+        return [n * 2024]
 
 
-iterations = 75
+iterations = 37
 
 @lru_cache(maxsize=None)
 def transform_stone_series(ss: int, ii: int) -> int:
@@ -41,14 +43,21 @@ def transform_stone_series(ss: int, ii: int) -> int:
     for _ in range(ii):
         temp = []
         for t in total:
-            temp.extend(transform(t))
+            temp += transform(t)
         total = temp
     return len(total)
 
+profiler = cProfile.Profile()
+profiler.enable()
 count = 0
 for s in stones:
     print(f'starting stone {s}')
-    count += transform_stone_series(s, 25)
+    
+    count += transform_stone_series(s, iterations)
     print(f'finished stone {s}')
+
+profiler.disable()
+profiler.dump_stats('stats')
+stats = pstats.Stats('stats').print_stats()
 
 print(count)
