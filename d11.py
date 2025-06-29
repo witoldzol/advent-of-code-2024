fda74c398ee8282c9490048569584d8c75a46d6b
Witold Zolnowski <blue3656@gmail.com>
2025-06-29 07:43:04 +0100

    clean up

--- d11.py
+++ d11.py
@@ -1,53 +1,66 @@
-import cProfile
-import pstats
-from collections import deque
 from functools import lru_cache
 
 
-stones = open('testinput').read().strip().split(' ')
+CACHE = {}
+
+stones = open("testinput").read().strip().split(" ")
 stones = open('input11').read().strip().split(' ')
 
-@lru_cache(maxsize=None)
+
+# @lru_cache(maxsize=None)
+def deep(n: int, iteration: int, max: int) -> int:
+    x = (n,iteration, max)
+    if x in CACHE:
+        return CACHE[x]
+    if iteration == max:
+        a = len(transform(n))
+        if x not in CACHE:
+            CACHE[(n,iteration, max)] = a
+        return a
+    else:
+        ans = transform(n)
+        if len(ans) == 2:
+            a, b = ans
+            ax = deep(a, iteration + 1, max)
+            if (a, iteration + 1, max) not in CACHE:
+                CACHE[(a, iteration + 1, max)] = ax
+            bx = deep(b, iteration + 1, max)
+            if (b, iteration + 1, max) not in CACHE:
+                CACHE[(b, iteration + 1, max)] = bx
+            return ax + bx
+        else:
+            aa = deep(ans[0], iteration + 1, max)
+            y = (ans[0], iteration + 1, max)
+            if y not in CACHE:
+                CACHE[y] = aa
+            return aa
+
+
 def transform(n: int) -> list[int]:
     if n == 0:
         return [1]
     # split into two halfs
     elif len(str(n)) % 2 == 0:
-        #print('splitting into two halfs num ' , str(n))
         num = str(n)
         mid = len(num) // 2
         left = num[:mid]
         if len(left) > 1:
-            left = left.lstrip('0')
-        if left == '':
+            left = left.lstrip("0")
+        if left == "":
             left = 0
-        #print('left ', left)
         right = num[mid:]
         if len(right) > 1:
-            right = right.lstrip('0')
-        if right == '':
+            right = right.lstrip("0")
+        if right == "":
             right = 0
-        #print('right ', right)
         return [int(left), int(right)]
     else:
-        # #print('multiply by 2024 ', n)
         return [n * 2024]
 
 
-iterations = 37
-
+iterations = 75
 count = 0
 for s in stones:
-    print(f'starting stone {s}')
-    total = []
-    total.append(int(s))
-    for _ in range(iterations):
-        temp = []
-        for t in total:
-            bob = transform(t)
-            temp += bob
-        total = temp
-    count += len(total)
-    print(f'finished stone {s}')
-
+    x = (int(s), 0, iterations-1)
+    count += deep(int(s), 0, iterations-1)
 print(count)
