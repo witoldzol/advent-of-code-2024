b257c3ef22de268fabe00af7465bc0153773747d
Witold Zolnowski <blue3656@gmail.com>
2025-10-14 07:46:06 +0100

    update test

---
+++ d12.py
@@ -0,0 +1,53 @@
+from pydantic import BaseModel
+import pytest
+# from d12 import map_fields, Field
+
+
+matrix = []
+with open("./testinput12") as f:
+    for line in f:
+        fields = list(line.strip())
+        matrix.append(fields)
+
+
+class Field(BaseModel):
+    area: int
+    permiter: int
+
+
+print(">" * 10)
+
+
+def map_fields(matrix: list[list[str]]) -> dict[str, list[Field]]:
+    area = {}
+    for row in matrix:
+        for cell in row:
+            if cell in area:
+                area[cell] += 1
+            else:
+                area[cell] = 1
+    results = {}
+    for k, v in area.items():
+        if v == 1:
+            p = 4
+        else:
+            p = 6
+        results[k] = [Field(area=v, permiter=p)]
+    return results
+
+
+########################################
+########################################
+########################################
+########################################
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        ([["A"]], {"A": [Field(area=1, permiter=4)]}),
+        ([["A", "A"]], {"A": [Field(area=2, permiter=6)]}),
+    ],
+)
+def test_one_field(input, expected):
+    assert map_fields(input) == expected
