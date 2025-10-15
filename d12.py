23cc9f3ef181c0fccb23265ffedc0a9fa4328cf9
Witold Zolnowski <blue3656@gmail.com>
2025-10-15 07:28:22 +0100

    test one column

--- d12.py
+++ d12.py
@@ -52,5 +52,16 @@ def map_fields(matrix: list[list[str]]) -> dict[str, list[Field]]:
         ([["A", "A", "A"]], {"A": [Field(area=3, permiter=8)]}),
     ],
 )
-def test_one_field(input, expected):
+def test_fields_in_1_row(input, expected):
+    assert map_fields(input) == expected
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        ([["A"], ["A"]], {"A": [Field(area=2, permiter=6)]}),
+        ([["A"], ["A"], ["A"]], {"A": [Field(area=3, permiter=8)]}),
+    ],
+)
+def test_field_in_1_column(input, expected):
     assert map_fields(input) == expected
